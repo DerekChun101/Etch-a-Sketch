a9c6b6ef1f23ef07514d750035b20d86f518c41a
Derek Chun <derekchun2013@gmail.com>
2023-11-07 21:37:40 -0500

    Added button that prompt user for size of grid

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-const grid = document.querySelector('#grid');
-for(i = 0;  i < 16; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.setAttribute('id', i);
-    square.addEventListener('mouseover', () => {
-        square.classList.add('painted');
-    })
-    grid.appendChild(square); 
+function createGrid(size) {
+    const grid = document.querySelector('#grid');
+    for(i = 0;  i < size * size; i++) {
+        const square = document.createElement('div');
+        const sizeOfSquare = 500/size; 
+        square.classList.add('square');
+        square.setAttribute('id', i);
+        square.style.height = `${sizeOfSquare}px`;
+        square.style.width = `${sizeOfSquare}px`;
+        square.addEventListener('mouseover', () => {
+            square.classList.add('painted');
+        });
+        
+        grid.appendChild(square); 
 
+    }
 }
+
+const sizeBtn = document.querySelector('#size-prompt');
+sizeBtn.addEventListener('click', () => {
+    const size = prompt('Set the size of the grid');
+    createGrid(size);
+})
 
 
 
