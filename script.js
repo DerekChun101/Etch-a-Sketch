b3cf5d524eb1256dce0138333b50e3689e7427b3
Derek Chun <derekchun2013@gmail.com>
2023-11-07 19:31:53 -0500

    Added abilty to paint squares use mouse over event to change background color of hovered over square divs

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const grid = document.querySelector('#grid');
 for(i = 0;  i < 16; i++) {
     const square = document.createElement('div');
-    square.setAttribute('class', 'square');
+    square.classList.add('square');
+    square.setAttribute('id', i);
+    square.addEventListener('mouseover', () => {
+        square.classList.add('painted');
+    })
     grid.appendChild(square); 
 
 }
+
+
 
 
     
